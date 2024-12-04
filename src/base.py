831e8fefebec0103f915dcaaf9554edf9bbf599d
araragitsubasa <67034838+ararakitsubasa@users.noreply.github.com>
2024-12-04 20:34:50 +0900

    remove

--- src/base.py
+++
@@ -1,8 +0,0 @@
-# base.py
-class Base:
-    def __init__(self, data):
-        self.data = data  # 关键数据
-        self.result = None  # 运算结果的存储位置
-
-    def show_data(self):
-        print(f"Data: {self.data}")
