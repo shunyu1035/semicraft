954ee999cfda6b79c4415afd4e8784f9f599b4e9
araragitsubasa <67034838+ararakitsubasa@users.noreply.github.com>
2024-12-23 12:29:20 +0900

    bug fixed, and parallel

--- src/etching_Cl_yield_kdtree.py
+++ src/etching_Cl_yield_kdtree.py
@@ -46,48 +46,31 @@ class etching(configuration, surface_normal):
         reactListAll = np.ones(indice_inject.shape[0])*-2
 
         pos_1 = self.parcel[indice_inject, :3]
-        vel_1 = self.parcel[indice_inject, 3:6]
-        ijk_1 = self.parcel[indice_inject, 6:9].astype(np.int32)
 
         if np.any(indice_inject):
-            # self.planes = self.get_pointcloud(sumFilm)
-            self.indice_inject = indice_inject
-
-            # get_theta = self.normal_matrix[ijk_1]
-            # get_plane = self.film[ijk_1]
-
+            
             # 可以把kdtree分散方法写在这里用作判断反应发生位置
             plane_bool = self.film_label_index_normal[:, :, :, 0] == 1
             # print(f'self.film_label_index_normas{self.film_label_index_normal[plane_bool]}')
             vacuum_bool = self.film_label_index_normal[:, :, :, 0] == -1
             get_plane, get_theta, get_plane_vaccum = self.get_inject_normal_kdtree(self.film_label_index_normal[plane_bool], self.film_label_index_normal[vacuum_bool], pos_1)
 
-            # reaction
-            self.film[get_plane[:,0], get_plane[:,1],get_plane[:,2]],\
-            self.film[get_plane_vaccum[:,0], get_plane_vaccum[:,1], get_plane_vaccum[:,2]], \
-            self.parcel[indice_inject,:], update_film_etch, update_film_depo, \
-            reactList, depo_parcel = \
-            reaction.reaction_rate(self.parcel[indice_inject], \
-                           self.film[get_plane[:,0], get_plane[:,1],get_plane[:,2]], \
-                           self.film[get_plane_vaccum[:,0], get_plane_vaccum[:,1], get_plane_vaccum[:,2]], \
-                           get_theta)
-            
-            # update film_label_index_normal
-            etch_bool =  update_film_etch.shape[0] > 0
-            depo_bool =  update_film_depo.shape[0] > 0
-            if etch_bool:
-                # a bug tobe fixed sometimes the film can not be etched when label is changed , maybe the reaction_rate func
-                self.film[get_plane[update_film_etch, 0], get_plane[update_film_etch, 1], get_plane[update_film_etch, 2], :] = 0
-                
-                point_etch_add_depo = np.zeros((get_plane[update_film_etch].shape[0] + get_plane[update_film_depo].shape[0], 3))
-                point_etch_add_depo[:update_film_etch.shape[0], :] = get_plane[update_film_etch]
-                # print(f'update_film_etch:{update_film_etch}')
-                self.film_label_index_normal, self.film_label_index_normal_mirror = self.update_film_label_index_normal_etch(self.film_label_index_normal_mirror, self.mirrorGap, get_plane[update_film_etch])
-            if depo_bool:
-                point_etch_add_depo[update_film_etch.shape[0]:, :] = get_plane[update_film_depo]
-                self.film_label_index_normal, self.film_label_index_normal_mirror = self.update_film_label_index_normal_depo(self.film_label_index_normal_mirror, self.mirrorGap, get_plane[update_film_depo])
-            if etch_bool | depo_bool:
-                # print(point_etch_add_depo)
+            # reaction parallel
+            self.film, self.parcel[indice_inject], update_film_etch, update_film_depo, reactList, depo_parcel = \
+            reaction.reaction_rate_parallel(self.film, self.parcel[indice_inject], get_plane, get_plane_vaccum, get_theta)
+
+            point_etch = self.film_label_index_normal[update_film_etch, 1:4]
+            point_depo = self.film_label_index_normal[update_film_depo, 1:4]
+            point_etch_add_depo = np.zeros((point_etch.shape[0] + point_depo.shape[0], 3))
+            if update_film_etch.any():
+                point_etch_add_depo[:point_etch.shape[0], :] = point_etch
+                self.film_label_index_normal, self.film_label_index_normal_mirror = \
+                    self.update_film_label_index_normal_etch(self.film_label_index_normal_mirror, self.mirrorGap, point_etch.astype(np.int64))
+            if update_film_depo.any():
+                point_etch_add_depo[point_etch.shape[0]:, :] = point_depo
+                self.film_label_index_normal, self.film_label_index_normal_mirror = \
+                    self.update_film_label_index_normal_depo(self.film_label_index_normal_mirror, self.mirrorGap, point_depo.astype(np.int64))
+            if update_film_etch.any() or update_film_depo.any():
                 self.film_label_index_normal_mirror = mirror.update_surface_mirror(self.film_label_index_normal, self.film_label_index_normal_mirror, self.mirrorGap, self.cellSizeX, self.cellSizeY)
                 self.film_label_index_normal = self.update_normal_in_matrix(self.film_label_index_normal_mirror, self.film_label_index_normal, self.mirrorGap, point_etch_add_depo.astype(np.int64))
                 # self.log.info('refreshFilm')
